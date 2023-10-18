1f2289581cefc2be5652d9e4af1c6a5a37bfccd6
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-18 12:05:14 +0100

    code

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+let size;
+
+if (arg.startsWith("0x")) {
+    // If the argument starts with "0x," assume it's a hexadecimal number
+    size = parseInt(arg, 16);
+} else {
+    // Otherwise, assume it's a decimal number
+    size = parseInt(arg, 10);
+}
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
